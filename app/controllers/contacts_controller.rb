130dcff53541c47cca2d307d4a59e107ffa7a076
azini <aazadi@gmail.com>
2014-09-13 14:51:28 +0200

    little fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,10 +2,24 @@ class ContactsController < ApplicationController
   def create
     from = params[:from]
     body = params[:body]
-    Pony.mail(:to => 'arianepauls@web.de',          :from => 'letter@ramble.berlin', :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
-    Pony.mail(:to => 'norbert.witzgall@gmail.com',  :from => 'letter@ramble.berlin', :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
-    Pony.mail(:to => 'mail@arianepauls.com',        :from => 'letter@ramble.berlin', :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
-    Pony.mail(:to => 'aazadi@gmail.com',            :from => 'letter@ramble.berlin', :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
+
+    #set the default options
+    Pony.options = {
+      :from => 'rambleberlin@gmail.com',
+      :via => :smtp,
+      :via_options => {
+          :address              => 'smtp.gmail.com',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'rambleberlin',
+          :password             => 'raemble@sept2014',
+          :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+          :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+        }    }
+    Pony.mail(:to => 'arianepauls@web.de',          :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
+    Pony.mail(:to => 'norbert.witzgall@gmail.com',  :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
+    Pony.mail(:to => 'mail@arianepauls.com',        :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
+    Pony.mail(:to => 'aazadi@gmail.com',            :subject => "[RAMBLE] new contact from #{from}", :body => "#{body}  -- #{from}")
 
     Contact.create!(from: from, body: body)
 
