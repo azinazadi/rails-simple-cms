007eb7bc995da47dce2cda40eb9a0a025950ceec
azini <aazadi@gmail.com>
2015-01-11 13:17:06 +0100

    better analytics

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -22,18 +22,25 @@ ActiveAdmin.register_page "Analytics" do
       tr do
         th 'ip'
         th 'count'
-        th 'os / entrance'
+        th 'os'
         th 'whats'
       end
-      Event.select(:ip).distinct.each do |event|
-        ip = event.ip
-        whats = Event.where(ip: ip).all.map(&:what).join(', ')
+      sorted_ips = Event.select(:ip).distinct.map(&:ip).sort_by { |ip| Event.find_by(ip: ip).created_at}
+      sorted_ips.reverse.each do |ip|
+        whats = Event.where(ip: ip).all.map do |event|
+          unless event.what=='contact'
+            event.what
+          else
+            c = Contact.find(event.data)
+            link_to("contact: #{c.from}", admin_contact_path(c), target: '_blank')
+          end
+        end.join(', ')
         count = Event.where(ip: ip).size
         tr do
-          td "<a href='/admin/events?ip_contains%5D=#{ip}'>#{ip}</a>".html_safe, style: 'width:15em'
+          td "<a href='/admin/events?utf8=✓&q%5Bip_contains%5D=#{ip}' target='_blank'>#{ip}</a>".html_safe, style: 'width:15em'
           td count
-          td Event.where(ip: ip).first.data[-4..-1]
-          td whats
+          td (Event.where(ip: ip).first.data || '' )[-4..-1]
+          td whats.html_safe
         end
 
       end
