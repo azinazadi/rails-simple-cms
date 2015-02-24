aab2d746ce0315f496d050842242f29741b9b161
azini <aazadi@gmail.com>
2015-02-24 01:09:18 +0100

    bugfix

--- app/controllers/forwards_controller.rb
+++ app/controllers/forwards_controller.rb
@@ -1,7 +1,7 @@
 class ForwardsController < ApplicationController
 
   def fwd
-    Event.create(ip: request.remote_ip, what: 'paid', data: slug)
+    Event.create(ip: request.remote_ip, what: 'paid', data: params['slug'])
     redirect_to Forward.find_by(slug: params['slug']).url
   end
 
